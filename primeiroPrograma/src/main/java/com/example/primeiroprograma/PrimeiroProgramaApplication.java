26b49c303a6b392d90117e2812e6fb13bfdc3cb4
Augusto <augustocesarrezende64@gmail.com>
2024-09-23 21:53:57 -0300

    primeiro exercicio realizado

---
+++ primeiroPrograma/src/main/java/com/example/primeiroprograma/PrimeiroProgramaApplication.java
@@ -0,0 +1,13 @@
+package com.example.primeiroprograma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProgramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProgramaApplication.class, args);
+	}
+
+}
